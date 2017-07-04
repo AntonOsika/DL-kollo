ca8439395b5b9c805ede01637596d7529506df56
Anton Osika <anton.osika@gmail.com>
2017-07-04 16:26:03 +0200

    Updated DKT and rule_based

--- kollo/models/DKT.py
+++ kollo/models/DKT.py
@@ -1,6 +1,10 @@
 import numpy as np
 
+import tensorflow as tf
+
 import keras
+import keras.backend as K
+from keras.layers.wrappers import TimeDistributed
 
 from kollo import simulators
 
@@ -10,22 +14,41 @@ np.set_printoptions(precision=2, suppress=True)
 
 se = simulators.StudentEnv()
 
-n_data = 2000
-data = []
-
 
 def models_fn():
-    shared_lstm = keras.layers.LSTM(4)
-    shared_dense1 = keras.layers.Dense(4, activation='sigmoid')
-    shared_dense2 = keras.layers.Dense(1)
+    shared_lstm = keras.layers.LSTM(4, return_sequences=True)
+    shared_dense1 = TimeDistributed(keras.layers.Dense(4, activation='sigmoid'))
+    shared_dense2 = TimeDistributed(keras.layers.Dense(1))
 
-    inp_train = keras.layers.Input([None, 3])
+    inp_train = keras.layers.Input([None, se.action_space, 3])
+
+    inputs_train = [ keras.layers.Lambda(lambda x: x[:, :, i, :])(inp_train) for i in range(se.action_space) ]
+
+    lstm_out_train = [shared_lstm(x) for x in inputs_train]
+    dense_out1_train = [(shared_dense1(x)) for x in lstm_out_train]
+    dense_out2_train = [(shared_dense2(x)) for x in dense_out1_train]
+
+    # Mask probabilities that are not used to zero. Takes X and prob.
+    mask_and_shift_layer = keras.layers.Lambda(lambda x: x[0][:, 1:, 0:1]*x[1][:, :-1])
+    out_train_list = [mask_and_shift_layer([y, z]) for y, z in zip(inputs_train, dense_out2_train)]
+
+    def concatenate_list(x):
+        # s = K.shape(x[0])
+        # s_list = [s[i] for i in range(s.shape[0].value)]
+        # s_list.insert(2, 1)
+        # shape = tf.stack(s_list)
+        # [tf.reshape(a, shape) for a in x]
+        return K.stack(x, axis=2)
+
+    out_train = keras.layers.Lambda(concatenate_list)(out_train_list)
 
 
-    lstm_out_train = shared_lstm(inp_train)
-    dense_out1_train = shared_dense1(lstm_out_train)
-    out_train = shared_dense2(dense_out1_train)
+    m_train = keras.models.Model(inp_train, out_train)
 
+    def loss(y, y_pred):
+        return K.mean(K.binary_crossentropy(y, y_pred, from_logits=True), axis=-1)
+
+    m_train.compile(loss=loss, optimizer='adam')
 
 
     inp_test = keras.layers.Input([None, se.action_space, 3])
@@ -37,48 +60,95 @@ def models_fn():
     dense_out1_test = [shared_dense1(x) for x in lstm_out_test]
     dense_out2_test = [shared_dense2(x) for x in dense_out1_test]
 
-    out_test = keras.layers.Concatenate()(dense_out2_test)
+    out_test = keras.layers.Lambda(concatenate_list)(dense_out2_test)
+
+    out_test_prob = keras.layers.Activation('sigmoid')(out_test)
+
+    m_test = keras.models.Model(inp_test, out_test_prob)
+    m_test.compile(loss='mse', optimizer='sgd')
 
 
 
-    return keras.models.Model(inp_train, out_train), keras.models.Model(inp_test, out_test),
+    return m_train, m_test
 
 
 
 
-train_model, test_model = models_fn()
+def default_dkt(last_embed_dim=200, n_units=4, dropout_input=0.0, dropout_recurrent=0.0, dropout_readout=0.25):
+
+    # Previously had one-hot transform as first layer, commented out:
+
+    # (Rows are selected not columns, this is one-hot transform equivalent)
+    # basis = np.concatenate((np.zeros((n_categories+1, 1)), np.eye(n_categories+1)), axis=1).T
+
+    inp = keras.models.Input((None, se.action_space, 3))
+    inp_reshaped = keras.layers.Reshape([-1, se.action_space*3])
+
+    # last_embedding = keras.layers.Embedding( 2*n_categories+2, last_embed_dim, mask_zero=True)(inputs[0])
+
+    lstm_output = keras.layers.recurrent.LSTM( n_units, return_sequences=True, recurrent_dropout=dropout_recurrent, dropout=dropout_input )(inp)
+
+    # dot product of next_embedding with LSTM output to get probability. Might be faster with dense layers to categorical crossentropy loss
+    next_indices = keras.layers.Lambda(lambda x: x[:, 1:])(inp)
+
+    next_embedding = keras.layers.Embedding( se.action_space, n_units, mask_zero=False)()
+
+
+    # TODO: Add 1 dim embedding for difficulty of skill. Use dropout on this to create "artificial" data
+
+    #p_hat = keras.layers.dot( [lstm_output, next_embedding], axes=[2, 2] )
+    lstm_output_dropout = keras.layers.Dropout(dropout_readout)(lstm_output)
+
+    logit = keras.layers.Lambda( lambda x: K.sum(x[0]*x[1], axis=2) ) ([lstm_output_dropout, inp])
+
+    p_hat = keras.layers.Activation(activation='sigmoid')(logit)
+
+    m = keras.models.Model(inputs=inp, outputs=p_hat)
+
+
+    m.compile('adam', 'binary_crossentropy', metrics=['acc'])
+    return m
+
 
 # print(model.summary())
 
 
+n_data = 200
+train_data = []
+
 
 for _ in range(n_data):
-    corrects = []
     done = False
     while not done:
-        obs, reward, done = se.do_exercise(np.random.randint(0, se.num_skills))
+        obs, reward, done, _ = se.step(np.random.randint(0, se.num_skills))
+    train_data.append(obs)
+    se.reset()
 
 
+def data_generator():
+    i = 0
+    while True:
+        yield [ train_data[i%len(train_data)][np.newaxis, :, :, :], train_data[i%len(train_data)][np.newaxis, :-1, :, 1:2] ]
+        i += 1
 
-cem = CEMAgent(model=model, nb_actions=se.action_space, memory=memory,
-               batch_size=50, nb_steps_warmup=2000, train_interval=50, elite_frac=0.05)
-cem.compile()
+X = [ d[np.newaxis] for d in train_data ]
 
-# Okay, now it's time to learn something! We visualize the training here for show, but this
-# slows down training quite a lot. You can always safely abort the training prematurely using
-# Ctrl + C.
-history = cem.fit(se, nb_steps=50000, visualize=False, verbose=2)
+Y = [ d[np.newaxis, :-1, :, 1:2] for d in train_data ]
 
-rewards = [ x for x in history.history['episode_reward'] if x > 0 ]
+train_model, test_model = models_fn()
 
-import matplotlib.pyplot as plt
-
-plt.plot(np.convolve(np.ones(100), rewards, 'valid'))
-plt.show()
+# history = train_model.fit_generator(data_generator(), steps_per_epoch=n_data, epochs=1)
+history = train_model.fit(np.array(X), np.array(Y), epochs=1)
 
 
-# After training is done, we save the best weights.
-cem.save_weights('cem_{}_params.h5f'.format('Student2'), overwrite=True)
+done = False
+while not done:
+    obs, reward, done, _ = se.step(np.random.randint(0, se.num_skills))
+    # print(test_model.predict(obs[np.newaxis]))
+    # print(train_model.predict(obs[np.newaxis]))
 
-# Finally, evaluate our algorithm for 5 episodes.
-cem.test(se, nb_episodes=5, visualize=False)
+avg_corr = np.sum(obs[:, :, 0]*obs[:, :, 1])/np.sum(obs[:, :, 0])
+
+avg_corr_pred = np.sum(test_model.predict(obs[np.newaxis])*obs[np.newaxis, :, :, 0:1])/np.sum(obs[:, :, 0])
+
+print("Reward: {} Average corr: {} Pred corr: {} ".format(reward, avg_corr, avg_corr_pred) )
--- kollo/models/rule_based.py
+++ kollo/models/rule_based.py
@@ -2,10 +2,14 @@ from kollo import simulators
 import  numpy as np
 import heapq
 
+# This benchmark uses rules (tuned only to the Student env, but tested on both Student and SimpleStudent) and evaluates the performance. 
+
+# Achieves 0.69 for Student (almost the same as perfect_information) and 0.51 for SimpleStudent.
 
 Student = simulators.Student
 
 rewards = np.zeros((20, 100))
+rewards2 = np.zeros((20, 100))
 
 inc_estimate_factor = 1.8
 double_half_life = 3.0
@@ -13,10 +17,7 @@ initial1 = 115
 initial2 = 35
 time_delta = 4.8
 
-# Loop for finding parameters:
-# for i, time_delta in enumerate(np.linspace(4, 5.5, 20)):
-for i in [0]:
-    s = Student()
+def evaluate_student(s, rewards, i):
 
     for j in range(100):
 
@@ -62,4 +63,15 @@ for i in [0]:
 
         s.reset()
 
-    print("{}: {} +- {}".format(time_delta, np.mean(rewards[i]), np.std(rewards[i])) )
+# Loop for finding parameters:
+# for i, time_delta in enumerate(np.linspace(4, 5.5, 20)):
+for i in [0]:
+    s = Student()
+    s2 = simulators.SimpleStudent()
+
+    evaluate_student(s, rewards, i)
+    evaluate_student(s2, rewards2, i)
+
+
+    print("Stud: {}: {} +- {}".format(time_delta, np.mean(rewards[i]), np.std(rewards[i])) )
+    print("Simple: {}: {} +- {}".format(time_delta, np.mean(rewards2[i]), np.std(rewards2[i])) )
